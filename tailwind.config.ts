6194ef8400787c4ca9a3ddd87554af8023d8c78a
Sebastian Pieczyński <testament777@gmail.com>
2023-09-14 11:21:06 +0200

    feat: ➕ configure NextUI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,15 @@
-import type { Config } from 'tailwindcss'
+import { nextui } from '@nextui-org/react';
+
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
 	content: [
 		'./content/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}', // for nextui
 	],
+	darkMode: 'class',
 	theme: {
 		// colors: {
 		// 	green: {
@@ -61,6 +65,6 @@ const config: Config = {
 			'spin-2s': 'spin 2s linear infinite',
 		},
 	},
-	plugins: [],
+	plugins: [nextui()],
 };
-export default config
+export default config;
