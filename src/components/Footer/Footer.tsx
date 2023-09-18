090cd84749f9aa716370d63b398c7c55cf814646
Sebastian Pieczynski <testament777@gmail.com>
2023-09-18 11:49:59 +0200

    refactor: 💄 change footer and sort tw classes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,74 +15,74 @@ function Footer({ className }: FooterProps) {
 	const currentYear = format(new Date(), 'yyyy');
 
 	return (
-		<div className={className}>
-			<div
-				className={
-					'md:flex-row md:justify-between md:items-baseline max-md:gap-8 relative flex justify-between flex-row gap-16 w-full max-w-content-width pb-12 max-xs:flex-col'
-				}
-			>
-				<div className="flex flex-col">
-					<Logo className={'text-xl'} />
-					<ul className={'mt-6 list-none p-0 last:mb-0'}>
-						<li className="mb-2">
-							<p className={'mt-0 mb-0 text-sm text-decoration'}>
-								Created by{' '}
-								<Link
-									href="/"
-									className={
-										'text-slate-900 no-underline font-semibold hover:underline hover:underline-offset-[0.125em] hover:decoration-2'
-									}
-								>
-									Sebastian Pieczyński
-								</Link>{' '}
-								&copy;{' '}
-								{SITE_PUBLISHED_YEAR_STRING === currentYear ? '' : '2023-'}
-								{currentYear}.{' '}
-							</p>
-						</li>
-						<li className="mb-2">
-							<p className={'mt-4 mb-0 text-xs text-decoration font-light'}>
-								Inspired by {` `}
-								<a
-									href="https://www.joshwcomeau.com/"
-									className={
-										'text-slate-900 font-semibold no-underline hover:underline hover:underline-offset-[0.125em] hover:decoration-2'
-									}
-								>
-									Josh W. Comeau
-								</a>
-								.
-							</p>
-						</li>
-					</ul>
-				</div>
+    <div className={className}>
+      <div
+        className={
+          "md:jstify-between max-w-content-width relative flex w-full flex-row justify-between gap-16 pb-12 max-md:gap-8 max-xs:flex-col md:flex-row md:items-baseline"
+        }
+      >
+        <div className="flex flex-col">
+          <Logo className={"text-xl"} />
+          <ul className={"list-none p-0 first:mt-6 last:mb-0"}>
+            <li className="mb-2">
+              <p className={"mb-0 mt-7 text-xs font-light text-decoration"}>
+                Inspired by {` `}
+                <a
+                  href="https://www.joshwcomeau.com/"
+                  className={
+                    "font-semibold text-slate-900 no-underline hover:underline hover:decoration-2 hover:underline-offset-[0.125em]"
+                  }
+                >
+                  Josh W. Comeau
+                </a>
+                .
+              </p>
+            </li>
+            <li className="mb-2">
+              <p className={"mb-0 mt-4 text-sm text-decoration"}>
+                Created by{" "}
+                <Link
+                  href="/"
+                  className={
+                    "font-semibold text-slate-900 no-underline hover:underline hover:decoration-2 hover:underline-offset-[0.125em]"
+                  }
+                >
+                  Sebastian Pieczyński
+                </Link>{" "}
+                &copy;{" "}
+                {SITE_PUBLISHED_YEAR_STRING === currentYear ? "" : "2023-"}
+                {currentYear}.{" "}
+              </p>
+            </li>
+          </ul>
+        </div>
 
-				<nav>
-					<h2 className={'mt-0 text-xl'}>Links</h2>
-					<ul className={'mt-6 list-none p-0 last:mb-0'}>
-						<li className="mb-2">
-							<Link className="no-underline text-base" href="/terms">
-								Terms of Use
-							</Link>
-						</li>
-						<li className="mb-2">
-							<Link className="no-underline text-base" href="/privacy">
-								Privacy Policy
-							</Link>
-						</li>
-						<li className="mb-2">
-							<a
-								className="no-underline text-base"
-								href={`https://twitter.com/${SOCIAL_X_HANDLE}`}
-							>
-								Twitter / X
-							</a>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-	);
+        <nav>
+          <h2 className={"mt-0 text-xl"}>Links</h2>
+          <ul className={"mt-6 list-none p-0 last:mb-0"}>
+            <li className="mb-2">
+              <Link className="text-base no-underline" href="/terms">
+                Terms of Use
+              </Link>
+            </li>
+            <li className="mb-2">
+              <Link className="text-base no-underline" href="/privacy">
+                Privacy Policy
+              </Link>
+            </li>
+            <li className="mb-2">
+              <a
+                className="text-base no-underline"
+                href={`https://twitter.com/${SOCIAL_X_HANDLE}`}
+              >
+                Twitter / X
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
