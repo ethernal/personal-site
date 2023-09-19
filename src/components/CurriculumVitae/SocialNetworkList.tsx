a50218ca7c4665dd97b569a0a0ad17b67c82e331
Sebastian Pieczynski <testament777@gmail.com>
2023-09-19 20:52:07 +0200

    feat: ✨ display social links from JSON Resume fields

---
+++ src/components/CurriculumVitae/SocialNetworkList.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {
+	RiFacebookLine,
+	RiGithubLine,
+	RiGitlabLine,
+	RiInstagramLine,
+	RiLinkedinLine,
+	RiTwitterXLine,
+} from 'react-icons/ri';
+
+import { ProfileLink } from './ProfileLink';
+
+const networkIconMap = {
+	twitter: RiTwitterXLine,
+	linkedin: RiLinkedinLine,
+	github: RiGithubLine,
+	facebook: RiFacebookLine,
+	instagram: RiInstagramLine,
+	gitlab: RiGitlabLine,
+};
+
+type SocialProfile = {
+	network: string;
+	username: string;
+};
+
+type SocialProfiles = {
+	network: string;
+	username: string;
+}[];
+
+type SocialNetworkListProps = {
+	socialProfiles: Prettify<SocialProfiles>;
+};
+
+function SocialNetworkList(props: SocialNetworkListProps) {
+	const { socialProfiles } = props;
+	console.log('Social: ', socialProfiles);
+	return socialProfiles.map((profile: SocialProfile) => {
+		const Icon = networkIconMap[profile.network];
+
+		return (
+			<div key={profile.network} className="flex gap-2 items-center">
+				<Icon />
+				<ProfileLink
+					className="no-underline"
+					network={profile.network}
+					username={profile.username ?? ''}
+				>
+					{profile.username}
+				</ProfileLink>
+			</div>
+		);
+	});
+}
+
+export default SocialNetworkList;
