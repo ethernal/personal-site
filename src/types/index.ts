14f1628b1ecfd85f072e73f67d8095a77dad87cc
Sebastian Pieczynski <testament777@gmail.com>
2023-09-24 07:10:56 +0200

    fix: 🐛 fix import bug in index of types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,2 @@
-// import BlogPostFrontmatterType from './BlogPostFrontmatterType';
-// import ColorThemeType from './ColorThemeType';
-
-export * from from './ColorThemeType';
-export * from from './BlogPostFrontmatterType';
-// export { BlogPostFrontmatterType, ColorThemeType };
+export * from './ColorThemeType';
+export * from './BlogPostFrontmatterType';
