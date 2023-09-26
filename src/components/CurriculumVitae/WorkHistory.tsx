8a31d4f0d5e67b750c41404cc05cbb41ec6cce1d
Sebastian Pieczynski <testament777@gmail.com>
2023-09-26 14:37:38 +0200

    refactor: 💄 use new WorkplaceCard for employers

--- src/components/CurriculumVitae/WorkHistory.tsx
+++ src/components/CurriculumVitae/WorkHistory.tsx
@@ -1,13 +1,11 @@
-import Link from 'next/link';
-import { TbWorldWww } from 'react-icons/tb';
+import { Workplace } from '@/types/CurriculumVitae';
+import { cn, getDataAttributesFromJSONKeywords } from '@/utils/utils';
 
-import { Employment } from '@/types/CurriculumVitae';
-
-import HorizontalItem from '../HorizontalItem';
+import WorkplaceCard from '../WorkplaceCard';
 
 type WorkHistoryProps = {
 	className?: string;
-	workHistory: Employment[];
+	workHistory: Workplace[];
 };
 
 export default function WorkHistory({
@@ -15,47 +13,15 @@ export default function WorkHistory({
 	workHistory: employmentHistory,
 }: WorkHistoryProps) {
 	return (
-		<section className={className}>
+		<section className={cn('grid grid-cols-1 gap-4', className)}>
 			{employmentHistory.map((workplace) => {
-				const {
-					name,
-					position,
-					website,
-					startDate,
-					endDate,
-					summary,
-					location,
-					highlights,
-					keywords,
-				} = workplace;
+				const { name, startDate, keywords } = workplace;
+
+				const dataKeywords = getDataAttributesFromJSONKeywords(keywords ?? []);
 
 				return (
-					<article key={`${workplace.name}-${workplace.startDate}`}>
-						<header>
-							<HorizontalItem as="div" className="items-baseline gap-6">
-								<p className={'text-xl font-semibold mb-0 mt-4'}>{name} </p>
-								<Link href={website} target="_blank">
-									<TbWorldWww />
-								</Link>
-							</HorizontalItem>
-							<h3 className="font-semibold text-lg mt-2 mb-0">{position}</h3>
-							<p className="text-small">
-								<time dateTime="YYYY-MM-DD">{startDate}</time> &ndash;{' '}
-								<time dateTime="YYYY-MM-DD">{endDate}</time>
-							</p>
-							<p>{location}</p>
-						</header>
-						<section>
-							<p>{summary}</p>
-							<p>{...highlights}</p>
-						</section>
-						<footer>
-							<ul>
-								{keywords?.map((keyword) => {
-									return <li key={`${name}-keyword-${keyword}`}>{keyword}</li>;
-								})}
-							</ul>
-						</footer>
+					<article key={`${name}-${startDate}`} {...dataKeywords}>
+						<WorkplaceCard workplace={workplace} />
 					</article>
 				);
 			})}
