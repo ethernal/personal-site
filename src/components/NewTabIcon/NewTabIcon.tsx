2891a310a03dada823a9d9a8e45f7e3176d2c256
Sebastian Pieczynski <testament777@gmail.com>
2023-11-01 11:48:02 +0100

    refactor: 💄 new icon for NewTabIcon component

--- src/components/NewTabIcon/NewTabIcon.tsx
+++ src/components/NewTabIcon/NewTabIcon.tsx
@@ -5,12 +5,14 @@ import { cn } from '@/utils/utils';
 
 function NewTabIcon({ className }: { className?: string }) {
 	return (
-		<NewTab
-			className={cn(
-				'text-theme-light-text-dark-muted dark:text-theme-dark-text-light-muted',
-				className,
-			)}
-		/>
+		<span className="inline-flex text-[0.9rem]">
+			<NewTab
+				className={cn(
+					'text-theme-light-text-dark-muted dark:text-theme-dark-text-light-muted',
+					className,
+				)}
+			/>
+		</span>
 	);
 }
 
