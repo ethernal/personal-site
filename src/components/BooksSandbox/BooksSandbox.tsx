9902d745667194add9ae7621af3a82191310c4e7
Sebastian Pieczynski <testament777@gmail.com>
2023-11-16 09:55:46 +0100

    feat: ✨ final version of the article

--- src/components/BooksSandbox/BooksSandbox.tsx
+++ src/components/BooksSandbox/BooksSandbox.tsx
@@ -2,62 +2,70 @@
 import React, { ChangeEvent, useState } from 'react';
 
 import { cn } from '@/utils/utils';
-import { CardHeader, Checkbox, CheckboxGroup, Image } from '@nextui-org/react';
+import { Checkbox, Image } from '@nextui-org/react';
 
 import CardUI, { CardUIBody, CardUIHeader } from '../CardUI';
 
 const response = [
 	{
 		title: 'The Way of the Kings',
-		order: 1,
 		author: 'Brandon Sanderson',
 		series: 'Stormlight Archive',
+		order: 1,
 		words: '383389',
 		publishedOn: '2010.08.31',
 		image:
-			'/image/article/using-key-when-mapping-through-list-of-items-in-react/way-of-the-kings.jpg',
+			'/image/article/why-do-i-need-key-when-mapping-through-list-of-items-in-react/way-of-the-kings.jpg',
 	},
 	{
 		title: 'Words of Radiance',
-		order: 2,
 		author: 'Brandon Sanderson',
 		series: 'Stormlight Archive',
+		order: 2,
 		words: '399431',
 		publishedOn: '2014.03.04',
 		image:
-			'/image/article/using-key-when-mapping-through-list-of-items-in-react/words-of-radiance.jpg',
+			'/image/article/why-do-i-need-key-when-mapping-through-list-of-items-in-react/words-of-radiance.jpg',
 	},
 	{
 		title: 'Oathbringer',
-		order: 3,
 		author: 'Brandon Sanderson',
 		series: 'Stormlight Archive',
+		order: 3,
 		words: '451912',
 		publishedOn: '2017.11.14',
+		image:
+			'/image/article/why-do-i-need-key-when-mapping-through-list-of-items-in-react/oathbringer.jpg',
 	},
 	{
 		title: 'Rythm of War',
-		order: 4,
 		author: 'Brandon Sanderson',
 		series: 'Stormlight Archive',
+		order: 4,
 		words: '455891',
 		publishedOn: '2020.11.17',
+		image:
+			'/image/article/why-do-i-need-key-when-mapping-through-list-of-items-in-react/rythm-of-war.jpg',
 	},
 	{
 		title: 'Knights of Wind and Truth',
-		order: 5,
 		author: 'Brandon Sanderson',
 		series: 'Stormlight Archive',
+		order: 5,
 		words: '400000',
 		publishedOn: '',
+		image:
+			'/image/article/why-do-i-need-key-when-mapping-through-list-of-items-in-react/szeth.webp',
 	},
 	{
 		title: 'Mistborn: The Final Empire',
-		order: 1,
 		author: 'Brandon Sanderson',
 		series: 'Mistborn',
+		order: 1,
 		words: '214000',
 		publishedOn: '2006-07-17',
+		image:
+			'/image/article/why-do-i-need-key-when-mapping-through-list-of-items-in-react/mistborn-final-empire.jpg',
 	},
 ];
 
@@ -66,24 +74,28 @@ const Book = React.memo(function Book({
 }: {
 	book: (typeof response)[0];
 }) {
-	console.log('Rendering book: ', book.title);
+	console.info('Rendering book: ', book.title);
 	return (
-		<CardUI key={book.title} className="max-w-[120px]">
-			<CardUIHeader>{book.title}</CardUIHeader>
+		<CardUI key={book.title} className="max-w-[300px]">
+			<CardUIHeader className="font-semibold font-heading tracking-widest flex gap-2 flex-row justify-center items-center">
+				{book.title}
+			</CardUIHeader>
 			<CardUIBody className="flex flex-col">
 				<Image
 					src={book?.image}
 					alt={book?.title}
 					classNames={{
-						img: 'object-cover aspect-square w-full',
+						wrapper: 'self-center',
+						img: 'object-cover aspect-square w-full max-h-[210px] ',
 					}}
 				/>
 				<ul>
 					{Object.entries(book).map(([key, value]) => {
-						if (key === 'title') return;
+						if (key === 'title' || key === 'image') return;
 						return (
 							<li key={key}>
-								{key}: {value}
+								<span className="font-semibold">{key}</span>:{' '}
+								<span className="">{value}</span>
 							</li>
 						);
 					})}
@@ -154,7 +166,7 @@ const BookList = ({ items = [] }: { items: typeof response | [] }) => {
 				<label htmlFor="mistborn">Mistborn</label>
 			</div>
 			<ul
-				className={`grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-4`}
+				className={`grid grid-cols-[repeat(auto-fit,minmax(280px,1fr))] gap-4`}
 			>
 				{books
 					.filter((book) => seriesFilter.includes(book.series))
@@ -168,7 +180,7 @@ const BookList = ({ items = [] }: { items: typeof response | [] }) => {
 
 export default function BooksSandbox() {
 	return (
-		<div className="App">
+		<div className="mb-8">
 			<h1 className="mb-4">Example</h1>
 			<BookList items={response} />
 		</div>
