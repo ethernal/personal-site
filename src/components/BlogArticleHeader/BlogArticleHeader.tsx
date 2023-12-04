c6e43d7cc17ea796e8a71cadf39c9cc479a07ce0
Sebastian Pieczynski <testament777@gmail.com>
2023-12-04 11:20:39 +0100

    allow adding className to BlogArticleHeader

--- src/components/BlogArticleHeader/BlogArticleHeader.tsx
+++ src/components/BlogArticleHeader/BlogArticleHeader.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+
+import { cn } from '@/utils/utils';
 
 export type BlogArticleHeaderProps = {
 	title: string;
-	publishedOn: string;
+	publishedOn?: string;
+	className?: string;
 };
 
-function BlogArticleHeader({ title, publishedOn }: BlogArticleHeaderProps) {
+function BlogArticleHeader({
+	title,
+	publishedOn,
+	className,
+}: BlogArticleHeaderProps) {
 	return (
-		<div className="mb-1">
+		<div className={cn('mb-1', className ?? '')}>
 			<h2 className="mb-1">{title}</h2>
 			{publishedOn !== undefined && publishedOn !== '' && (
 				<p>Published on: {new Date(publishedOn).toLocaleDateString()}</p>
 			)}
 		</div>
 	);
-
 }
 
 export default BlogArticleHeader;
